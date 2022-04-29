36483c17331a4c78e08dbabb0dc53856549ca471
log-info <heechan.yang@navercorp.com>
2022-04-30 03:23:30 +0900

    first react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(props) {
+  return <header>
+    <h1><a href='/'>WEB</a></h1>
+  </header>
+}
+
+function ListElement(props) {
+  return <li><a id={props.id} href={'/read/' + props.id} 
+    onClick={(event) => {
+      event.preventDefault();
+      props.onchangeMode(event.target.id);
+    }}>{props.title}</a></li>
+}
+
+function Navigation(props) {
+  const list = []
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i]
+    list.push(<ListElement key={t.id} id={t.id} title={t.title} onchangeMode={props.onchangeMode}/>)
+
+  }
+  return <nav>
+    <ol>
+      {list}
+    </ol>
+  </nav>
+}
+
+function Article(props) {
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
+
 function App() {
+  const topics = [
+    { id: 1, title: 'html', body: 'html is ...' },
+    { id: 2, title: 'css', body: 'cs is ...' },
+    { id: 3, title: 'js', body: 'js is ...' },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Navigation topics={topics} onchangeMode={(id) => {
+        alert(id);
+      }} />
+      <Article title="Welcome" body="Hello, React!!" />
     </div>
   );
 }
